ed534892c3f745338d00a1fb62d9c16ec7f9b070
shappoff <shappoff@gmail.com>
2024-02-05 06:42:55 +0000

    Update from https://github.com/shappoff/metrics-src/commit/992e897b875ad85a62a969df86d2cf469f7469b6

---
+++ metrics/components/BornMetrics.d.ts
@@ -0,0 +1,3 @@
+import { default as React } from 'react';
+declare const BornMetrics: ({ hits }: any) => React.JSX.Element;
+export default BornMetrics;
---
+++ metrics/components/TooltipNoteOverlay.d.ts
@@ -0,0 +1,3 @@
+import { default as React } from 'react';
+declare const TooltipNoteOverlay: ({ value, note, dotsWidth }: any) => React.JSX.Element;
+export default TooltipNoteOverlay;
