ac68ece3e7f743db6cc78e27965a0100db3ed908
shappoff <shappoff@gmail.com>
2024-02-05 09:41:14 +0000

    Update from https://github.com/shappoff/ispoved-src/commit/78dc61dd8fa8202e9637fcc05002c89cf602a84b

---
+++ ispoved/components/LeafletInfoMap.d.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet.markercluster';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+import 'leaflet-sidebar';
+import 'leaflet-sidebar/src/L.Control.Sidebar.css';
+import 'leaflet-search';
+import 'leaflet-search/src/leaflet-search.css';
+export declare const LeafletInfoMap: React.FC<any>;
