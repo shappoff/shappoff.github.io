0019623d6916496261e78c2cf33aeac3a6001694
shappoff <shappoff@gmail.com>
2024-04-23 12:54:27 +0000

    Update from https://github.com/shappoff/map/commit/cc4d8eeb7d83617bc7de06face04f615d546dd32

---
+++ map/components/MapApp.d.ts
@@ -0,0 +1,6 @@
+import React from "react";
+import "leaflet/dist/leaflet.css";
+import "leaflet-draw/dist/leaflet.draw.css";
+import 'leaflet-sidebar';
+import 'leaflet-sidebar/src/L.Control.Sidebar.css';
+export declare const MapApp: () => React.JSX.Element;
---
+++ map/components/icons.d.ts
@@ -0,0 +1,6 @@
+export declare const pinIcon: () => string;
+export declare const circleIcon: ({ iconColor }: any) => string;
+export declare const pilIcon: ({ iconColor }: any) => string;
+export declare const houseIcon: ({ iconColor }: any) => string;
+export declare const schoolIcon: ({ iconColor }: any) => string;
+export declare const getIcon: (type: string) => ({ iconColor }: any) => string;
