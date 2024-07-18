1f95160d3bcdd379ca2005d5cde48597d95501ef
shappoff <shappoff@gmail.com>
2024-07-18 11:55:04 +0000

    Update from https://github.com/shappoff/glinninki1846/commit/1a19e1e61c63ad3c891b1fd63de0d947b066cd0f

---
+++ glinniki1846/components/MapApp.d.ts
@@ -0,0 +1,7 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "leaflet/dist/leaflet.css";
+import "leaflet-draw/dist/leaflet.draw.css";
+export declare const MapApp: () => React.JSX.Element;
+export declare function get(obj: any, propPath: string, defaultValue?: any): any;
+export declare function getPropertyByPath(obj: any, propPath: string, defaultValue?: any): any;
---
+++ glinniki1846/components/icons.d.ts
@@ -0,0 +1,7 @@
+export declare const pinIcon: () => string;
+export declare const circleIcon: ({ iconColor }: any) => string;
+export declare const pilIcon: ({ iconColor }: any) => string;
+export declare const houseIcon: ({ iconColor }: any) => string;
+export declare const schoolIcon: ({ iconColor }: any) => string;
+export declare const arrowUpIcon: () => string;
+export declare const getIcon: (type: string) => string | (({ iconColor }: any) => string);
