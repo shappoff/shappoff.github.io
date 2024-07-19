c8496c62226305598d5e5f73e562a0b2bdec1f42
shappoff <shappoff@gmail.com>
2024-07-19 20:49:13 +0000

    Update from https://github.com/shappoff/glinninki1846/commit/94a147ae19f269f18e8ba847ff4e014c552af512

--- glinniki1846/items.d.ts
+++ glinniki1846/items.d.ts
@@ -3,14 +3,23 @@ export declare const items: ({
     _geoloc: number[];
     info?: undefined;
     place?: undefined;
+    value?: undefined;
 } | {
     title: string;
     _geoloc: number[];
     info: string;
     place?: undefined;
+    value?: undefined;
 } | {
     title: string;
     _geoloc: number[];
     info: string;
     place: string;
+    value: string;
+} | {
+    title: string;
+    _geoloc: number[];
+    info: string;
+    place: string;
+    value?: undefined;
 })[];
