3c97bea6e0d9844e7f3e474b57ea8bbf5d3b1952
Sergey Shappo <shappoff@gmail.com>
2024-07-30 09:16:16 +0300

    Delete map directory

--- map/components/icons.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const pinIcon: () => string;
-export declare const circleIcon: ({ iconColor }: any) => string;
-export declare const pilIcon: ({ iconColor }: any) => string;
-export declare const houseIcon: ({ iconColor }: any) => string;
-export declare const schoolIcon: ({ iconColor }: any) => string;
-export declare const getIcon: (type: string) => ({ iconColor }: any) => string;
