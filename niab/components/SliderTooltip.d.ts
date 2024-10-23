5948f7d6943f1d645bb3d1903088e222697d2966
shappoff <shappoff@gmail.com>
2024-10-23 12:49:52 +0000

    Update from https://github.com/shappoff/foandyniab/commit/261b941a482cab898a408460a4892a125041f66a

---
+++ niab/components/SliderTooltip.d.ts
@@ -0,0 +1,7 @@
+import React from "react";
+interface SliderTooltipProps {
+    children: React.ReactNode;
+    theme?: any;
+}
+declare const SliderTooltip: React.FC<SliderTooltipProps>;
+export default SliderTooltip;
