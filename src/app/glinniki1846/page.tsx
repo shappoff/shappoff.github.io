aa467a19f67ef170ff09149bc3c9c6d49e0c6caa
Sergey Shappo <shappoff@gmail.com>
2024-12-27 14:59:57 +0300

    add glinniki1846

--- src/app/glinniki1846/page.tsx
+++ src/app/glinniki1846/page.tsx
@@ -1,18 +1,11 @@
-'use client'
-
 import {Metadata} from "next";
-// import {MapApp} from "./MapApp";
-import dynamic from 'next/dynamic'
+import MapApp from "@/app/glinniki1846/MapApp";
 
-const DynamicComponentWithNoSSR = dynamic(() => import('./MapApp'), { ssr: false });
-
-/*
 export const metadata: Metadata = {
     title: "1846 год, им. Глинник. Масштаб 1/8400.",
     description: "Лесохозяйственный план государственного имения Глинник. Масштаб 1/8400. Могилевский уезд, Могилевская губерния",
 };
-*/
 
 export default function Glinniki1846() {
-  return <DynamicComponentWithNoSSR />;
+  return <MapApp />;
 }
