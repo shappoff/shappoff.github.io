389a64e98e7ce2d0b3026c6cf838e0d07dd1482d
Sergey Shappo <shappoff@gmail.com>
2025-02-01 00:50:02 +0300

    fav icon

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -6,7 +6,7 @@ export const size = {
     height: 32,
 }
 export const contentType = 'image/png'
-
+export const dynamic = 'force-static';
 // Image generation
 export default function Icon() {
     return new ImageResponse(
