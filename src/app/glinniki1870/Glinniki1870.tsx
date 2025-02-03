b8cf14a479dac8f3cecaa2d4a8842024706f6aab
Sergey Shappo <shappoff@gmail.com>
2025-02-04 00:00:04 +0300

    fix map

--- src/app/glinniki1870/Glinniki1870.tsx
+++ src/app/glinniki1870/Glinniki1870.tsx
@@ -11,25 +11,25 @@ import {
 const Glinniki1870 = () => {
 
     return <MapContainer
-        attributionControl={false}
         id="map"
-        key="map"
         center={[0, 0]}
         zoom={2}
-        trackResize={true}
-        scrollWheelZoom={true}
-        zoomControl={false}
+        zoomControl={true}
         style={{height: '99vh'}}
+        whenReady={() => {
+            setTimeout(() => {
+                document!.querySelector('.leaflet-control-attribution.leaflet-control')!
+                    .innerHTML = '<div>Деревня Глинники. Генеральное межевание, НИАБ-2192-1-55</div>';
+            }, 100);
+        }}
     >
-        <LayerGroup>
-            <TileLayer
-                attribution="Дер. Глинники. Генеральное межевание"
-                url={`https://raw.githubusercontent.com/shappoff/storage/glinniki1870/tiles/{z}/{y}/{x}.jpg`}
-                maxZoom={6}
-                minZoom={2}
-                noWrap={true}
-            />
-        </LayerGroup>
+        <TileLayer
+            attribution="Деревня Глинники. Генеральное межевание, НИАБ-2192-1-55"
+            url={`https://raw.githubusercontent.com/shappoff/storage/glinniki1870/tiles/{z}/{y}/{x}.jpg`}
+            maxZoom={6}
+            minZoom={2}
+            noWrap={true}
+        />
     </MapContainer>;
 };
 
