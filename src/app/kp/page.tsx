0e6144b4d54c80cb97c47e60803ee88a14e9316c
Sergey Shappo <shappoff@gmail.com>
2025-02-09 16:27:39 +0300

    add KP

--- src/app/kp/page.tsx
+++ src/app/kp/page.tsx
@@ -1,18 +1,21 @@
+import styles from "../page.module.css";
 import Link from "next/link";
 
 export default function KP() {
     return (
-        <>
-            <h1 id="книги-память">Книги Память:</h1>
-            <p><Link href="/kp/tolochinskiy">Память. Толочинский район</Link></p>
+        <div className={styles.page}>
+            <main className={styles.main}>
+                <h1 id="книги-память">Книги Память:</h1>
+                <p><Link href="/kp/tolochinskiy">Память. Толочинский район</Link></p>
 
-            <p><Link href="/kp/krupskiy">Память. Крупский район</Link></p>
+                <p><Link href="/kp/krupskiy">Память. Крупский район</Link></p>
 
-            <p><Link href="/kp/beshenkovichskiy">Память. Бешенковичский район</Link></p>
+                <p><Link href="/kp/beshenkovichskiy">Память. Бешенковичский район</Link></p>
 
-            <p><Link href="/kp/berezovskiy">Память. Берёзовский район</Link></p>
+                <p><Link href="/kp/berezovskiy">Память. Берёзовский район</Link></p>
 
-            <p><Link href="/kp/sennenskiy">Память. Сенненский район</Link></p>
-        </>
+                <p><Link href="/kp/sennenskiy">Память. Сенненский район</Link></p>
+            </main>
+        </div>
     );
 }
