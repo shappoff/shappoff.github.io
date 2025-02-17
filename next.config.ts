bd0843fc13b5f52e15f703458a7903df140453d9
Sergey Shappo <shappoff@gmail.com>
2025-02-17 22:59:20 +0300

    rm basePath

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    basePath: '/nextff',
     async redirects() {
         return [
             {
                 source: '/novosady',
-                destination: 'https://indexby.github.io/novosady/',
-                permanent: true,
-            },
-            {
-                source: '/nextff/novosady',
                 destination: 'https://indexby.github.io/novosady/',
                 permanent: true,
             },
