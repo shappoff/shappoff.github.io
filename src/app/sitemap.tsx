e5fbb3645bbe054c1fcbe12b3954fd779466dc7f
Sergey Shappo <shappoff@gmail.com>
2025-02-17 16:05:33 +0300

    add seo

---
+++ src/app/sitemap.tsx
@@ -0,0 +1,50 @@
+export const dynamic = 'force-static';
+
+
+export default async function sitemap() {
+
+    return [
+        {
+            url: 'shappoff.github.io',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://shappoff.github.io/zhigalo',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+        {
+            url: 'https://shappoff.github.io/glinniki1870',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://shappoff.github.io/glinniki1846',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://shappoff.github.io/niab',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://shappoff.github.io/kp',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://shappoff.github.io/prikhody',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+    ]
+}
