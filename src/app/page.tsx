06a2f4236bb4fbea27a9e37dd3f8a9c32dd421d7
Sergey Shappo <shappoff@gmail.com>
2025-02-17 16:11:50 +0300

    update link titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <Link href="/zhigalo">Семен Жигало. Дневник.</Link>
-        <Link href="/copy">Копии документов.</Link>
-        <Link href="/fond">Документы для просмотра.</Link>
-        <Link href="/glinniki1870">План д. Глинники, 1870 г..</Link>
-        <Link href="/glinniki1846">План имения Глинники, 1846 г.</Link>
-        <Link href="/niab">Фонды и описи НИАБ.</Link>
-        <Link href="/kp">Книги Память</Link>
-        <Link href="/prikhody">Приходы Беларуси</Link>
+        <Link href="/copy">Копии документов что есть у меня.</Link>
+        <Link href="/fond">Документы, которые я планирую смотреть в архивах.</Link>
+        <Link href="/glinniki1870">План д. Глинники, 1870 г., Могилевская губерния, Могилевский уезд, Павловичская волость</Link>
+        <Link href="/glinniki1846">План имения Глинники, 1846 г. Могилевская губерния, Могилевский уезд, Павловичская волость</Link>
+        <Link href="/niab">Фонды и описи НИАБ. Удобный поиск описей по фондам НИАБ (Национальный исторический архив Беларуси).</Link>
+        <Link href="/kp">Книги Память.</Link>
+        <Link href="/prikhody">Приходы Беларуси. Карта церквей и костелов. Сохранность метрических записей.</Link>
       </main>
       <footer className={styles.footer}>
 
