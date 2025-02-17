d60e6c02d3be3e3dea36ecb032994a131d2d4e12
Sergey Shappo <shappoff@gmail.com>
2025-02-17 23:39:58 +0300

    fix css

--- src/app/niab/page.tsx
+++ src/app/niab/page.tsx
@@ -1,6 +1,7 @@
 import {Metadata} from "next";
 import FondyNIABApp from "@/components/niab/FondyNIABApp";
 import './NIAB.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const metadata: Metadata = {
     title: "Фонды и описи НИАБ",
@@ -21,12 +22,6 @@ export const metadata: Metadata = {
 
 export default function NIAB() {
   return <>
-      <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-          crossOrigin="anonymous"
-      />
       <FondyNIABApp/>
   </>;
 }
