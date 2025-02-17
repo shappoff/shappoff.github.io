36038415b63f19c8e49870826e101854079b8ac1
Sergey Shappo <shappoff@gmail.com>
2025-02-17 15:23:56 +0300

    add redirects

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     basePath: '/nextff',
+    async redirects() {
+        return [
+            {
+                source: '/novosady',
+                destination: 'https://indexby.github.io/novosady/',
+                permanent: true,
+            },
+        ]
+    },
 };
 
 export default nextConfig;
