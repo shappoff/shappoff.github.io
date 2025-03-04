cb54a89954b30418ea71126eba3c03fa70c4ebc3
Sergey Shappo <shappoff@gmail.com>
2025-03-04 12:39:35 +0300

    add all

--- src/app/catalogarchivesgov/page.tsx
+++ src/app/catalogarchivesgov/page.tsx
@@ -1,7 +1,7 @@
 import {Metadata} from "next";
 import Dynamic from "@/components/catalogarchivesgov/Dynamic";
 import fs from "fs";
-import {Belarus_full, smolensk_full} from "@/components/paths";
+import {Belarus_full, latvia_full, lithuania_full, russia_full, smolensk_full, ukraine_full} from "@/components/paths";
 
 export const metadata: Metadata = {
     title: "Карта аэрофотосъемки Беларуси времен ВОВ",
@@ -11,5 +11,16 @@ export const metadata: Metadata = {
 export default function CatalogarchivesgovPage() {
     const belarusItems = JSON.parse(fs.readFileSync(Belarus_full, 'utf8'));
     const smolenskItems = JSON.parse(fs.readFileSync(smolensk_full, 'utf8'));
-    return <Dynamic items={[...belarusItems, ...smolenskItems]}/>;
+    const latvia_fullItems = JSON.parse(fs.readFileSync(latvia_full, 'utf8'));
+    const lithuania_fullItems = JSON.parse(fs.readFileSync(lithuania_full, 'utf8'));
+    const ukraine_fullItems = JSON.parse(fs.readFileSync(ukraine_full, 'utf8'));
+    const russia_fullItems = JSON.parse(fs.readFileSync(russia_full, 'utf8'));
+    return <Dynamic items={[
+        ...belarusItems,
+        ...smolenskItems,
+        ...latvia_fullItems,
+        ...lithuania_fullItems,
+        ...ukraine_fullItems,
+        ...russia_fullItems,
+    ]}/>;
 }
