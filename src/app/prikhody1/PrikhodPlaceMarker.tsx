9e59fa6460be92d05be7c5e9491fe982524a788e
Sergey Shappo <shappoff@gmail.com>
2025-03-06 10:16:57 +0300

    add CyrillicToTranslit

--- src/app/prikhody1/PrikhodPlaceMarker.tsx
+++ src/app/prikhody1/PrikhodPlaceMarker.tsx
@@ -3,11 +3,15 @@ const {
     DivIcon
 } = L;
 import {Marker, Popup, Tooltip} from "react-leaflet";
-import React from "react";
 import {catholicCrossIcon, ortodoxCrossIcon} from "@/components/icons";
+import CyrillicToTranslit from 'cyrillic-to-translit-js';
+import Link from "next/link";
+const cyrillicToTranslit: any = new (CyrillicToTranslit as any);
 
 const PrikhodPlaceMarker = ({hit, popupclose, popupopen, setCurrentLocIdInPopUp, selectCallback, children}: any) => {
     const [objectID, title, pTitle, pType, lat, lng, src, atd] = hit;
+    const linksID = atd?.split('|')
+        .map((value: string) => cyrillicToTranslit.transform(value.trim(), '_').toLowerCase())
     const isOrtodox = !!~title.indexOf('церковь');
     return <Marker title={`${pType ? `${pType} ` : ''}${pTitle}, ${title}`}
                    eventHandlers={{
@@ -25,13 +29,13 @@ const PrikhodPlaceMarker = ({hit, popupclose, popupopen, setCurrentLocIdInPopUp,
                        className: `marker-church-div-icon ${isOrtodox ? 'orthodox' : 'сatholic'} ${+src === 0 ? 'no-metrics' : ''}`
                    })}
                    position={[parseFloat(lat.replace(',', '.')), parseFloat(lng.replace(',', '.'))]}>
-        <Popup>
+        <Popup key="Popup">
             <b style={{textTransform: 'capitalize', whiteSpace: 'nowrap'}}>{pType ? `${pType} ` : ''}{pTitle}</b>
             <h6 style={{textTransform: 'capitalize'}}>{title}</h6>
             {children}
-            <p>
-                <a href="javascript:void(0)" onClick={() => {selectCallback(hit)}}><i>Подробнее</i></a>
-            </p>
+            {
+                linksID?.map((linkId: string) => <p><Link key={linkId} href={`/prikhody1/${linkId}`}>{linkId}</Link></p>)
+            }
         </Popup>
     </Marker>
 };
