bfc166a57fa32e6ede5a2496631099dce0764ab0
Sergey Shappo <shappoff@gmail.com>
2025-03-10 09:00:37 +0300

    Revert "update"
    This reverts commit b680f3c864385afd41cc758db99bb6db78b82a58.

--- src/app/prikhody1/layout.tsx
+++ src/app/prikhody1/layout.tsx
@@ -1,11 +1,10 @@
 import Prikhody from "@/app/prikhody1/Prikhody";
 import fs from "fs";
 import {prikhodyMainDataPath} from "@/components/paths";
-import nextConfig from "../../../next.config";
 
 export default function Layout({ children }: any) {
 
-    console.log('next-env.d.ts', nextConfig);
+
     const allPrikhods = JSON.parse(fs.readFileSync(prikhodyMainDataPath, 'utf8'));
 
     return (
--- src/scripts/pre-build/showNextjsConfig.ts
+++
@@ -1,9 +0,0 @@
-import path from "path";
-import fs from "fs";
-import nextConfig from "../../../next.config";
-
-function run() {
-    console.log('next-env.d.ts', nextConfig);
-}
-
-export default run;
