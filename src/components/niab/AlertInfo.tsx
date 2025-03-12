56561f6f67c0a7262f021e3d3a7556cd2e9a607b
Sergey Shappo <shappoff@gmail.com>
2025-03-12 08:24:29 +0300

    update info

---
+++ src/components/niab/AlertInfo.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import * as React from 'react';
+import Snackbar, {SnackbarCloseReason} from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Alert from '@mui/material/Alert';
+
+export default function AlertInfo({children, severity, anchorOrigin}: any) {
+    const [open, setOpen] = React.useState<boolean>(true);
+
+    const handleClose = (
+        event: React.SyntheticEvent | Event,
+        reason?: SnackbarCloseReason,
+    ) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    return <Snackbar
+        anchorOrigin={anchorOrigin}
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+    >
+        <Alert severity={severity}>
+            {children}
+            <IconButton
+                size="small"
+                aria-label="close"
+                color="inherit"
+                onClick={handleClose}
+            >
+                <CloseIcon fontSize="small"/>
+            </IconButton>
+        </Alert>
+    </Snackbar>;
+}
