07d725e99db18c0993db4b77e42b0d4d636f90f2
Sergey Shappo <shappoff@gmail.com>
2025-03-12 08:23:43 +0300

    update info

--- src/app/niab/layout.tsx
+++ src/app/niab/layout.tsx
@@ -1,9 +1,10 @@
-import ColsedNIABInfo from "@/components/niab/ColsedNIABInfo";
+import AlertInfo from "@/components/niab/AlertInfo";
+import * as React from "react";
 
 export default function Layout({ children }: any) {
     return (
         <>
-            <ColsedNIABInfo />
+            <AlertInfo severity="info" anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}>Количество записей в базе: Минск 808053 Гродно 196073</AlertInfo>
             <>{children}</>
         </>
     )
