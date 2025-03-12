7e148b0a719a5076621c9891790a54a1935a1d2e
Sergey Shappo <shappoff@gmail.com>
2025-03-12 09:31:57 +0300

    update infoPage

--- src/app/prikhody1/prkhd/[prikhod]/InfoPage.tsx
+++ src/app/prikhody1/prkhd/[prikhod]/InfoPage.tsx
@@ -1,26 +1,29 @@
 'use client'
 
 import Drawer from '@mui/material/Drawer';
-import { createPortal } from 'react-dom';
+import {createPortal} from 'react-dom';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 
 const InfoPage = () => {
     return <>
-        {createPortal(<>
-            <Drawer open={true} onClose={() => history.back()}  variant="persistent" anchor="bottom">
-                <div>
-                    <IconButton aria-label="delete" onClick={() => history.back()}>
-                        <CloseIcon />
-                    </IconButton>
-                </div>
-                <div>This child is placed in the document body.</div>
-                <div>This child is placed in the document body.</div>
-                <div>This child is placed in the document body.</div>
-                <div>This child is placed in the document body.</div>
-            </Drawer>
-
-        </>, document.getElementById('slide-panel-info'))}
+        {
+            createPortal(<>
+                    <Drawer open={true} variant="persistent" anchor="bottom">
+                        <div>
+                            <IconButton aria-label="delete" onClick={() => history.back()}>
+                                <CloseIcon/>
+                            </IconButton>
+                        </div>
+                        <div>This child is placed in the document body.</div>
+                        <div>This child is placed in the document body.</div>
+                        <div>This child is placed in the document body.</div>
+                        <div>This child is placed in the document body.</div>
+                    </Drawer>
+                </>,
+                document?.getElementById('slide-panel-info') as any
+            )
+        }
     </>
 };
 export default InfoPage;
