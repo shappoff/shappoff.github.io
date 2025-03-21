b57c5e6b3cdd59a797eb71db5ef532721b744700
Sergey Shappo <shappoff@gmail.com>
2025-03-21 13:55:44 +0300

    update table

---
+++ src/app/prikhody/p/[prikhod]/DataTable.tsx
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import Paper from '@mui/material/Paper';
+import CopyToClipboardData from "@/app/prikhody/p/[prikhod]/CopyToClipboardData";
+import Tooltip from '@mui/material/Tooltip';
+import Link from "next/link";
+import {getNestedArrayValue} from "@/components/utils";
+import DocumentScannerOutlinedIcon from '@mui/icons-material/DocumentScannerOutlined';
+import LinkIcon from '@mui/icons-material/Link';
+
+const paginationModel = { page: 0, pageSize: 10 };
+
+export default function DataTable({data, digited}: any) {
+    const columns: GridColDef[] = [
+        { field: 'copy', headerName: 'скопировать', width: 120,
+            renderCell: (params) => {
+                const {year, type, short, fod, link, full, fond, opis, delo, id} = params.row;
+                return <CopyToClipboardData data={`${short} ${fod}, ${type}, ${year}`} />;
+            }
+        },
+        { field: 'short', headerName: 'архив', width: 130,
+            renderCell: (params) => {
+                const {year, type, short, fod, link, full, fond, opis, delo, id} = params.row;
+                return <Tooltip arrow title={full}><u>{short}</u></Tooltip>;
+            }
+
+        },
+        { field: 'year', headerName: 'год', width: 130 },
+        {
+            field: 'type',
+            headerName: 'тип',
+            description: 'РБУ - Рождения, Браки, Умершие; ИВ - Исповедные Ведомости; БО - Брачные Обыски',
+            sortable: false,
+            type: 'string',
+            width: 200,
+        },
+        {
+            field: 'fod',
+            headerName: 'Ф-О-Д',
+            description: 'Фонд-Опись-Дело',
+            width: 160,
+            renderCell: (params) => {
+                const {year, type, short, fod, link, full, fond, opis, delo, id} = params.row;
+                return <React.Fragment><Link target="_blank" href={`/niab/${fond}`}><u>{fond}</u></Link>-<u>{opis}</u>-<u>{delo}</u></React.Fragment>;
+            }
+        },
+        {
+            field: 'note',
+            headerName: 'информация',
+            sortable: false,
+            width: 160,
+            renderCell: (params) => {
+                const {year, type, short, fod, link, full, fond, opis, delo, id} = params.row;
+                return <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-around', height: '100%'}}>
+                    {
+                        link ? <>
+                            <Tooltip arrow title="Ссылка на снимки">
+                                <Link target="_blank" href={link}>
+                                    <LinkIcon/>
+                                </Link>
+                            </Tooltip>
+                        </> : <></>}
+                    {
+                        getNestedArrayValue(digited, fond, opis, delo) ? <>
+                            <Tooltip arrow title="Оцифрованно в НИАБ">
+                                <DocumentScannerOutlinedIcon sx={{ fontSize: 15, cursor: 'pointer' }} />
+                            </Tooltip>
+                        </> : ''
+                    }
+                </div>;
+            }
+        },
+    ];
+
+    return (
+        <Paper sx={{width: '100%' }}>
+            <DataGrid
+                rows={data}
+                columns={columns}
+                initialState={{ pagination: { paginationModel } }}
+                pageSizeOptions={[5, 10, 50, 100]}
+                sx={{ border: 0 }}
+            />
+        </Paper>
+    );
+}
--- src/app/prikhody/p/[prikhod]/InfoPage.tsx
+++ src/app/prikhody/p/[prikhod]/InfoPage.tsx
@@ -4,10 +4,8 @@ import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import {useRouter} from 'next/navigation'
-import {Button, Table, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
-import LinkIcon from '@mui/icons-material/Link';
+import {Button} from "@mui/material";
 import React from "react";
-import Link from "next/link";
 import CreatePortalWrapper from "@/components/CreatePortalWrapper";
 import Box from '@mui/material/Box';
 import './InfoPage.css';
@@ -15,7 +13,6 @@ import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-import Tooltip from '@mui/material/Tooltip';
 import useFirebaseAuth from "@/components/prikhody/useFirebaseAuth";
 import {getDatabase, ref} from "firebase/database";
 import {useList} from "react-firebase-hooks/database";
@@ -23,9 +20,7 @@ import NPPlaceMarker from "@/components/prikhody/NPPlaceMarker";
 import SendArchivesData from "@/app/prikhody/p/[prikhod]/SendArchivesData";
 import useMarkersBounds from "@/components/prikhody/useMarkersBounds";
 import BoundsToMapItems from "@/components/prikhody/BoundsToMapItems";
-import CopyToClipboardData from "@/app/prikhody/p/[prikhod]/CopyToClipboardData";
-import {getNestedArrayValue} from "@/components/utils";
-import DocumentScannerOutlinedIcon from '@mui/icons-material/DocumentScannerOutlined';
+import DataTable from "@/app/prikhody/p/[prikhod]/DataTable";
 
 const InfoPage = ({archives, prikhod, digited}: any) => {
     const [objectID, title, pTitle, pType, lat, lng, src, atd] = prikhod;
@@ -112,59 +107,11 @@ const InfoPage = ({archives, prikhod, digited}: any) => {
                                 </Box>
                                 <TabPanel value={1}>
                                     <SendArchivesData objectID={objectID} />
-                                    <Table stickyHeader>
-                                        <TableHead>
-                                            <TableRow>
-                                                <TableCell align="center">скопировать</TableCell>
-                                                <TableCell align="center">архив</TableCell>
-                                                <TableCell align="center">год</TableCell>
-                                                <TableCell align="center">
-                                                    <Tooltip arrow title="РБУ - Рождения, Браки, Умершие; ИВ - Исповедные Ведомости; БО - Брачные Обыски">
-                                                        <u>тип</u>
-                                                    </Tooltip>
-                                                    </TableCell>
-                                                <TableCell align="center"><Tooltip arrow title="Фонд-Опись-Дело"><u>Ф-О-Д</u></Tooltip></TableCell>
-                                                <TableCell align="center">информация</TableCell>
-                                            </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                            {
-                                                archives?.map((aRow: any, indexItem: number) => {
-                                                    const [year, type, short, fod, link, full] = aRow;
-                                                    const [fond, opis, delo] = fod.split('-');
-                                                    return <TableRow key={indexItem}>
-                                                        <TableCell align="center">
-                                                            <CopyToClipboardData data={`${short} ${fod}, ${type}, ${year}`} />
-                                                        </TableCell>
-                                                        <TableCell align="center">
-                                                            <Tooltip arrow title={full}><u>{short}</u></Tooltip>
-                                                        </TableCell>
-                                                        <TableCell align="center">{year}</TableCell>
-                                                        <TableCell align="center">{type}</TableCell>
-                                                        <TableCell align="center">
-                                                            <Link target="_blank" href={`/niab/${fond}`}><u>{fond}</u></Link>-<u>{opis}</u>-<u>{delo}</u>
-                                                        </TableCell>
-                                                        <TableCell align="center">
-                                                            {
-                                                                link ? <>
-                                                                    <Tooltip arrow title="Ссылка на снимки">
-                                                                        <Link target="_blank" href={link}>
-                                                                            <LinkIcon/>
-                                                                        </Link>
-                                                                    </Tooltip>
-                                                                </> : <></>}
-                                                            {
-                                                                getNestedArrayValue(digited, fond, opis, delo) ? <>
-                                                                    <Tooltip arrow title="Оцифрованно в НИАБ">
-                                                                        <DocumentScannerOutlinedIcon sx={{ fontSize: 15, cursor: 'pointer' }} />
-                                                                    </Tooltip>
-                                                                </> : ''
-                                                            }
-                                                        </TableCell>
-                                                    </TableRow>
-                                                })
-                                            }</TableBody>
-                                    </Table>
+                                    <DataTable digited={digited} data={archives.map((aRow: Array<any>, index: number) => {
+                                        const [year, type, short, fod, link, full] = aRow;
+                                        const [fond, opis, delo] = fod.split('-');
+                                        return ({year, type, short, fod, link, full, fond, opis, delo, id: index});
+                                    })} />
                                 </TabPanel>
                                 <TabPanel value={2}>
                                     <ol>
