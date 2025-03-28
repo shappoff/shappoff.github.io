19d8fd034efe565b035cbdd5dd98fc60edd1e6cd
Sergey Shappo <shappoff@gmail.com>
2025-03-28 22:13:45 +0300

    add digited page

---
+++ src/app/prikhody/digited/page.tsx
@@ -0,0 +1,67 @@
+import {Metadata} from "next";
+import fs from "fs";
+import {
+    digitedFormattedDataPath,
+    prikhodyArchivesDataPath,
+    prikhodyMainDataPath,
+} from "@/components/paths";
+import WrapToMarkerClusterGroup from "@/app/prikhody/WrapToMarkerClusterGroup";
+
+export const metadata: Metadata = {
+    title: 'Оцифрованные дела | Карта приходов',
+    description: 'Оцифрованные дела церквей и костелов Беларуси. Генеалогия. Сохранность',
+    other: {
+        robots: "index, follow",
+        charset: "UTF-8",
+        image: '/map-icon.jpg',
+        url: 'https://shappoff.github.io/prikhody',
+        type: 'website'
+    },
+    keywords: ['Карта', 'Беларусь', 'Церкви', 'Костелы', 'генеалогия', 'Сохранность'],
+    robots: { index: true, follow: true },
+    icons: [
+        {
+            url: '/map-icon.svg',
+            type: 'image/svg+xml',
+            sizes: 'any',
+            rel: 'icon'
+        }
+    ],
+
+};
+
+export default function PrikhodyMapPage() {
+    const allPrikhods = JSON.parse(fs.readFileSync(prikhodyMainDataPath, 'utf8'));
+    const digitedFormattedData = JSON.parse(fs.readFileSync(digitedFormattedDataPath, 'utf8'));
+    const prikhodyArchivesData = JSON.parse(fs.readFileSync(prikhodyArchivesDataPath, 'utf8'));
+
+    const digited = allPrikhods.filter((prikhodItem: any) => {
+        let isDigited = false;
+        const [id] = prikhodItem;
+
+        if (prikhodyArchivesData[id]) {
+            const archivesArr = prikhodyArchivesData[id];
+            archivesArr.map((archiveItem: any) => {
+                const [year, title, short, fod] = archiveItem;
+                if (fod && short === 'НИАБ') {
+                    const [f, o, d] = fod.split('-');
+                    if (digitedFormattedData[f]) {
+                        if (digitedFormattedData[f][o]) {
+                            if (digitedFormattedData[f][o][d]) {
+                                isDigited = true;
+                            }
+                        }
+                    }
+
+                }
+            });
+        }
+
+        return isDigited;
+    });
+
+
+    return <>
+        <WrapToMarkerClusterGroup items={digited} bounds={false} />
+    </>
+}
