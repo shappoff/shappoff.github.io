eb0fe1573db83ef758f7c2d0864d3c2d7464b7d8
Sergey Shappo <shappoff@gmail.com>
2025-03-29 22:08:37 +0300

    add markerLabel

--- src/app/prikhody/MarkersList.tsx
+++ src/app/prikhody/MarkersList.tsx
@@ -3,15 +3,16 @@
 import PrikhodPlaceMarker from "@/app/prikhody/PrikhodPlaceMarker";
 import Link from "next/link";
 
-const Markers = ({items}: any) => {
+const Markers = ({items, markerLabel}: any) => {
     const isDev = !!~location.search.indexOf('debug');
     return items.map((hit: any, indexMarker: number) => {
         const [objectID, title, pTitle, pType, lat, lng, src, atd] = hit;
         return lat && lng ? <PrikhodPlaceMarker
-            key={objectID + indexMarker}
+            key={objectID}
             hit={hit}
             isMobile={true}
             isDev={isDev}
+            markerLabel={markerLabel}
         >
             <div>
                 <Link href={`/prikhody/p/${objectID}`}><big>Подробнее</big></Link>
