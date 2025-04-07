aec7c533a05b7a717756e2fdbabd1bd75d6d8829
Sergey Shappo <shappoff@gmail.com>
2025-04-07 21:32:41 +0300

    update pdf card

--- src/components/niab/OpisCard.tsx
+++ src/components/niab/OpisCard.tsx
@@ -5,7 +5,8 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Link from "next/link";
-import {Chip, Tooltip} from "@mui/material";
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import WrapToTooltip from "@/components/niab/BasicTooltip";
 
 export default function OpisCard({info}: any) {
     return (
@@ -23,16 +24,11 @@ export default function OpisCard({info}: any) {
         }}>
             <CardContent>
                 <Typography variant="h6" component="div" sx={{whiteSpace: 'nowrap'}}>Опись {info?.op}</Typography>
-                {
-                    info.s ? <>
-                        <Link href="https://forum.vgd.ru/post/468/118798/p3690535.htm#pp3690535" target="_blank">
-                            <Tooltip title="Индексация описей фондов НИАБ (Минск). Волонтерская инициатива. Присоединяйтесь к нашему проекту! Принять участие смогут все желающие">
-                                <Chip label={info.s + '%'} size="small" sx={{textDecoration: 'underline'}} />
-                            </Tooltip>
-                        </Link>
-                    </> : <></>
-                }
-                {info?.n ? <Typography sx={{color: 'text.secondary', mb: 1.5}}>{info?.n}</Typography> : <></>}
+                <WrapToTooltip note={info?.n}>
+                    <Link target="_blank" href={`https://drive.google.com/file/d/${info?.docId}`}>
+                        <PictureAsPdfIcon sx={{ fontSize: 70 }} />
+                    </Link>
+                </WrapToTooltip>
             </CardContent>
             <CardActions>
                 {info?.docId ? <Button size="small"><Link target="_blank" href={`https://drive.google.com/file/d/${info?.docId}`}>Открыть</Link></Button> : <></>}
