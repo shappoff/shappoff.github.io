ae8a90ceeaf6b5606cd54166b6f3b8abb8275c8b
Sergey Shappo <shappoff@gmail.com>
2025-04-10 23:24:00 +0300

    update

--- src/app/prikhody/PrikhodyMapApp.tsx
+++ src/app/prikhody/PrikhodyMapApp.tsx
@@ -24,6 +24,7 @@ import useDebounce from "@/components/useDebounce";
 
 import algoliasearch from 'algoliasearch/lite';
 import WrapToMarkerClusterGroup from "@/app/prikhody/WrapToMarkerClusterGroup";
+import {useGAnalytics} from "@/components/useGAnalytics";
 declare const process: any;
 
 const client = algoliasearch(
@@ -55,6 +56,7 @@ const PrikhodyMapApp = ({children, items}: any) => {
 
     const size = useWindowSize();
     const router = useRouter();
+    useGAnalytics('G-D24N93Y7CD');
     const debouncedSearchTerm = useDebounce(searchTerm, 1000);
 
     const [rootWith, setRootWith] = React.useState(0);
