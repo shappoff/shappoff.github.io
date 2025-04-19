f9f9e8222d50e694745277ebeaabc2f5a264373c
Sergey Shappo <shappoff@gmail.com>
2025-04-19 23:40:32 +0300

    update

--- src/app/niab/layout.tsx
+++ src/app/niab/layout.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 export default function Layout({ children }: any) {
     return (
         <>
-            <AlertInfo severity="info" anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}>Количество записей в базе: Минск 815401 Гродно 200388</AlertInfo>
+            <AlertInfo severity="info" anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}>Количество записей в базе: Минск 816389 Гродно 201590</AlertInfo>
             <>{children}</>
         </>
     )
