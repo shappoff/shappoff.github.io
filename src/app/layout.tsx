a5bccda3cfdb68e11a5b542bd75a9bf2243e14a7
Sergey Shappo <shappoff@gmail.com>
2025-04-23 15:24:51 +0300

    fix accesability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type {Metadata, Viewport} from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const viewport: Viewport = {
   width: 'device-width',
@@ -36,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         {children}
       </body>
     </html>
