c6f7dd3ef6265d8cdb8431fe59aac8615a91d357
Sergey Shappo <shappoff@gmail.com>
2025-05-18 00:31:04 +0300

    Update Page404.tsx

--- src/components/Page404.tsx
+++ src/components/Page404.tsx
@@ -6,8 +6,8 @@ const Page404 = ({routeList}: any) => {
     React.useLayoutEffect(() => {
         const {origin, pathname, href, search} = location;
         if (pathname.endsWith('/') && routeList.some((route: string) => pathname.endsWith(`${route}/`))) {
-            // location.replace(`${origin}${pathname.slice(0, -1)}${search}`);
-            history.replaceState(null, document.title, `${pathname.slice(0, -1)}${search}`)
+            location.replace(`${origin}${pathname.slice(0, -1)}${search}`);
+            // history.replaceState(null, document.title, `${pathname.slice(0, -1)}${search}`)
         }
         if (pathname === '/novosady') {
             location.href = 'https://indexby.github.io/novosady/';
