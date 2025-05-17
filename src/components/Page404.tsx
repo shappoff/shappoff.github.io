91e51c19463baf1d5d3221fdbab9acf3f0fc56bc
Sergey Shappo <shappoff@gmail.com>
2025-05-18 00:00:08 +0300

    replaceState

--- src/components/Page404.tsx
+++ src/components/Page404.tsx
@@ -5,8 +5,9 @@ import React from "react";
 const Page404 = ({routeList}: any) => {
     React.useLayoutEffect(() => {
         const {origin, pathname, href, search} = location;
-        if (pathname.endsWith('/') && routeList.some((route: string) => pathname.endsWith(`${route}/`))) {
-            location.href = `${origin}${pathname.slice(0, -1)}${search}`;
+        if (pathname.endsWith('/') && routeList.some((route: string) => pathname.endsWith(`${route}/`)) || pathname.endsWith('test/')) {
+            // location.replace(`${origin}${pathname.slice(0, -1)}${search}`);
+            history.replaceState(null, document.title, `${pathname.slice(0, -1)}${search}`)
         }
         if (pathname === '/novosady') {
             location.href = 'https://indexby.github.io/novosady/';
