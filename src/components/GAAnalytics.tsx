43cf45bf8b975e44b23fa79ca17748a3343505a6
Sergey Shappo <shappoff@gmail.com>
2025-07-05 17:43:34 +0300

    add types GAAnalytics.tsx

--- src/components/GAAnalytics.tsx
+++ src/components/GAAnalytics.tsx
@@ -3,8 +3,11 @@
 import React from 'react';
 import { GoogleAnalytics } from '@next/third-parties/google';
 
-const GAAnalytics: React.FC = () => {
+interface GAAnalyticsProps {}
+
+const GAAnalytics: React.FC<GAAnalyticsProps> = (): React.JSX.Element => {
     const [ga, setGa] = React.useState<boolean>(false);
+
     React.useEffect(() => {
         if (~location.href.indexOf('debug')) {
             localStorage.setItem('debug', 'true');
