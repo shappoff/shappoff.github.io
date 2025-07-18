b9d1ebc6a3a5a94e079a8106049d97ae50b049ef
Sergey Shappo <shappoff@gmail.com>
2025-07-18 16:27:49 +0300

    fix animation

--- src/components/niab/FondCard.tsx
+++ src/components/niab/FondCard.tsx
@@ -23,76 +23,84 @@ const FondCard = ({item, index}: any) => {
     // https://niab.by/newsite/ru/Priostanovka_hkranilische4
     const isntZal: any = !!fourthStorage[item.fod];
 
-    return <Card key={item.objectID} style={{animationDelay: `${index + 1}00ms`}}>
-        {
-            item.s ?
-            <div className="percentage-box" title={`${item.s} %`}>
-                <div className="empty-line">
-                    <div title={`Проиндексирован на ${item.s}%`} className="percentage-line" style={{
-                        width: `${Math.round(item.s)}%`
-                    }}><span className="percens">{`${item.s} %`}</span></div>
-                </div>
-            </div>
-                : <></>
-        }
-        <Card.Title>
-            <h5>
-                <a
-                    href={`/niab/${item.fod}`}
-                    className="fond-link-src">
-                    Фонд {item.fod}
-                </a>
+    const styleAnimationDelay = index < 12 ? {animationDelay: `${index + 1}00ms`} : {animationDelay: `${index + 1200}ms`};
 
-                <span className="copy-fond-icon" title="Скопировать ссылку на фонд" onClick={() => {
-                    const copyLink = `${location.origin}${location.pathname}${item.fod}`;
-                    try {
-                        navigator.clipboard.writeText(copyLink);
-                    } catch (err) {
-                        console.error('Failed to copy: ', err);
-                    }
-                }}>
+    return <Card key={item.objectID} style={styleAnimationDelay}>
+            {
+                item.s ?
+                    <div className="percentage-box" title={`${item.s} %`}>
+                        <div className="empty-line">
+                            <div title={`Проиндексирован на ${item.s}%`} className="percentage-line" style={{
+                                width: `${Math.round(item.s)}%`
+                            }}><span className="percens">{`${item.s} %`}</span></div>
+                        </div>
+                    </div>
+                    : <></>
+            }
+        <Card.Title>
+                <h5>
+                    <a
+                        href={`/niab/${item.fod}`}
+                        className="fond-link-src">
+                        Фонд {item.fod}
+                    </a>
+
+                    <span className="copy-fond-icon" title="Скопировать ссылку на фонд" onClick={() => {
+                        const copyLink = `${location.origin}${location.pathname}${item.fod}`;
+                        try {
+                            navigator.clipboard.writeText(copyLink);
+                        } catch (err) {
+                            console.error('Failed to copy: ', err);
+                        }
+                    }}>
                 <svg x="0px" y="0px" height="10px" width="10px" viewBox="0 0 115.77 122.88">
                     <g>
-                        <path className="st0" d="M89.62,13.96v7.73h12.19h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02v0.02 v73.27v0.01h-0.02c-0.01,3.84-1.57,7.33-4.1,9.86c-2.51,2.5-5.98,4.06-9.82,4.07v0.02h-0.02h-61.7H40.1v-0.02 c-3.84-0.01-7.34-1.57-9.86-4.1c-2.5-2.51-4.06-5.98-4.07-9.82h-0.02v-0.02V92.51H13.96h-0.01v-0.02c-3.84-0.01-7.34-1.57-9.86-4.1 c-2.5-2.51-4.06-5.98-4.07-9.82H0v-0.02V13.96v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07V0h0.02h61.7 h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02V13.96L89.62,13.96z M79.04,21.69v-7.73v-0.02h0.02 c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v64.59v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h12.19V35.65 v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07v-0.02h0.02H79.04L79.04,21.69z M105.18,108.92V35.65v-0.02 h0.02c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v73.27v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h61.7h0.02 v0.02c0.91,0,1.75-0.39,2.37-1.01c0.61-0.61,1-1.46,1-2.37h-0.02V108.92L105.18,108.92z"/>
+                        <path className="st0"
+                              d="M89.62,13.96v7.73h12.19h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02v0.02 v73.27v0.01h-0.02c-0.01,3.84-1.57,7.33-4.1,9.86c-2.51,2.5-5.98,4.06-9.82,4.07v0.02h-0.02h-61.7H40.1v-0.02 c-3.84-0.01-7.34-1.57-9.86-4.1c-2.5-2.51-4.06-5.98-4.07-9.82h-0.02v-0.02V92.51H13.96h-0.01v-0.02c-3.84-0.01-7.34-1.57-9.86-4.1 c-2.5-2.51-4.06-5.98-4.07-9.82H0v-0.02V13.96v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07V0h0.02h61.7 h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02V13.96L89.62,13.96z M79.04,21.69v-7.73v-0.02h0.02 c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v64.59v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h12.19V35.65 v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07v-0.02h0.02H79.04L79.04,21.69z M105.18,108.92V35.65v-0.02 h0.02c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v73.27v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h61.7h0.02 v0.02c0.91,0,1.75-0.39,2.37-1.01c0.61-0.61,1-1.46,1-2.37h-0.02V108.92L105.18,108.92z"/>
                     </g>
                 </svg>
             </span>
-            </h5>
-            <div>
-                {isntZal ? <Badge bg="danger" pill><Link target="_blank" href="https://niab.by/newsite/ru/Priostanovka_hkranilische4">Не выдается c 01.10.2024</Link></Badge> : <></>}
-                {item.storage ? <Badge bg="light" pill text="dark">Хранилище №{item.storage}</Badge> : <></>}
-                {item.count ? <Badge bg="light" pill text="dark">{item.count} ед. хр.</Badge> : <></>}
-                {
-                    item.lang?.map((ln: string) => <Badge key={ln} bg="light" pill text="dark">
-                        {ln}
-                    </Badge>)
-                }
-                {item.years ? <Badge bg="light" pill text="dark">{item.years}</Badge> : <></>}
-            </div>
+                </h5>
+                <div>
+                    {isntZal ? <Badge bg="danger" pill><Link target="_blank"
+                                                             href="https://niab.by/newsite/ru/Priostanovka_hkranilische4">Не
+                        выдается c 01.10.2024</Link></Badge> : <></>}
+                    {item.storage ? <Badge bg="light" pill text="dark">Хранилище №{item.storage}</Badge> : <></>}
+                    {item.count ? <Badge bg="light" pill text="dark">{item.count} ед. хр.</Badge> : <></>}
+                    {
+                        item.lang?.map((ln: string) => <Badge key={ln} bg="light" pill text="dark">
+                            {ln}
+                        </Badge>)
+                    }
+                    {item.years ? <Badge bg="light" pill text="dark">{item.years}</Badge> : <></>}
+                </div>
         </Card.Title>
         <Card.Body>
-            <Accordion activeKey={currentAccordion ?? ''} flush>
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header onClick={() => setCurrentAccordion((prevState: string) => prevState === '0' ? '' : '0')}><i>Название фонда</i></Accordion.Header>
-                    <Accordion.Body>
-                        <div dangerouslySetInnerHTML={{__html: state.title}}/>
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header onClick={() => setCurrentAccordion((prevState: string) => prevState === '1' ? '' : '1')}><i>Аннотация
-                        документов</i></Accordion.Header>
-                    <Accordion.Body>
-                        <div dangerouslySetInnerHTML={{__html: state.anotation}}/>
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Button size="small"
-                        variant="outlined"
-                        className="fond-opis-button-src"
-                        fullWidth={true}
-                        startIcon="Описи"
-                        endIcon={<ArrowForwardIosIcon />}
-                        href={`/niab/${item.fod}`} />
-            </Accordion>
+                <Accordion activeKey={currentAccordion ?? ''} flush>
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header
+                            onClick={() => setCurrentAccordion((prevState: string) => prevState === '0' ? '' : '0')}><i>Название
+                            фонда</i></Accordion.Header>
+                        <Accordion.Body>
+                            <div dangerouslySetInnerHTML={{__html: state.title}}/>
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="1">
+                        <Accordion.Header
+                            onClick={() => setCurrentAccordion((prevState: string) => prevState === '1' ? '' : '1')}><i>Аннотация
+                            документов</i></Accordion.Header>
+                        <Accordion.Body>
+                            <div dangerouslySetInnerHTML={{__html: state.anotation}}/>
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Button size="small"
+                            variant="outlined"
+                            className="fond-opis-button-src"
+                            fullWidth={true}
+                            startIcon="Описи"
+                            endIcon={<ArrowForwardIosIcon/>}
+                            href={`/niab/${item.fod}`}/>
+                </Accordion>
         </Card.Body>
     </Card>
 };
