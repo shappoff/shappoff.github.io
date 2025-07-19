ce3b491d7e95d7acfae06ddc1895908e75c7fa54
Sergey Shappo <shappoff@gmail.com>
2025-07-19 21:06:58 +0300

    fix CardBody.tsx

--- src/components/niab/CardBody.tsx
+++ src/components/niab/CardBody.tsx
@@ -4,28 +4,38 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-const CardBody = ({state}: any) => {
-    const {title, anotation} = state;
-    const isTitle = !!~title.indexOf('<b>');
-    const isAnotation = !!~anotation.indexOf('<b>');
+const CardBody = ({state: stateProps}: any) => {
+    const [state, setState] = React.useState();
+
+    React.useEffect(() => {
+        const {title, anotation} = stateProps;
+        const isTitle = !!~title.indexOf('<b>');
+        const isAnotation = !!~anotation.indexOf('<b>');
+
+        setState({isTitle, isAnotation});
+    }, [stateProps]);
+
+    if (!state) {
+        return <></>
+    }
 
     return <>
-        <Accordion defaultExpanded={isTitle}>
+        <Accordion expanded={state.isTitle}>
             <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
                 <header>
                     <i>Название фонда</i></header>
             </AccordionSummary>
             <AccordionDetails>
-                <p dangerouslySetInnerHTML={{__html: state.title}}/>
+                <p dangerouslySetInnerHTML={{__html: stateProps.title}}/>
             </AccordionDetails>
         </Accordion>
-        <Accordion defaultExpanded={isAnotation}>
+        <Accordion expanded={state.isAnotation}>
             <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
                 <header>
                     <i>Аннотация документов</i></header>
             </AccordionSummary>
             <AccordionDetails>
-                <p dangerouslySetInnerHTML={{__html: state.anotation}}/>
+                <p dangerouslySetInnerHTML={{__html: stateProps.anotation}}/>
             </AccordionDetails>
         </Accordion>
     </>
