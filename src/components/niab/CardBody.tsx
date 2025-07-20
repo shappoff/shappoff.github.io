4589d8100dfb7213d043f8ba05497c24f20678b8
Sergey Shappo <shappoff@gmail.com>
2025-07-20 12:11:09 +0300

    fix CardBody expand

--- src/components/niab/CardBody.tsx
+++ src/components/niab/CardBody.tsx
@@ -4,52 +4,26 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-type StateType = {
-    isTitle: boolean
-    isAnotation?: boolean
-};
-
-type StatePropsType = {
-    state: {
-        title: string;
-        anotation: string;
-    }
-};
-
-const CardBody = ({state: stateProps}: StatePropsType) => {
-    const [state, setState] = React.useState<StateType>();
-
-    React.useEffect(() => {
-        const {title, anotation} = stateProps;
-        const isTitle = !!~title.indexOf('<b>');
-        const isAnotation = !!~anotation.indexOf('<b>');
-
-        setState({isTitle, isAnotation});
-    }, [stateProps]);
-
-    if (!state) {
-        return <></>
-    }
-
+const CardBody = ({state}: any) => {
     return <>
-        <Accordion expanded={state.isTitle}>
+        <Accordion defaultExpanded={state.isTitle}>
             <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
                 <header>
                     <i>Название фонда</i>
                 </header>
             </AccordionSummary>
             <AccordionDetails>
-                <p dangerouslySetInnerHTML={{__html: stateProps.title}}/>
+                <p dangerouslySetInnerHTML={{__html: state.title}}/>
             </AccordionDetails>
         </Accordion>
-        <Accordion expanded={state.isAnotation}>
+        <Accordion defaultExpanded={state.isAnotation}>
             <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
                 <header>
                     <i>Аннотация документов</i>
                 </header>
             </AccordionSummary>
             <AccordionDetails>
-                <p dangerouslySetInnerHTML={{__html: stateProps.anotation}}/>
+                <p dangerouslySetInnerHTML={{__html: state.anotation}}/>
             </AccordionDetails>
         </Accordion>
     </>
