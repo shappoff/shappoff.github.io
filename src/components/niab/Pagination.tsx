eeacc19b2ed2ab6f5df1bd4841b191dd367f195a
Sergey Shappo <shappoff@gmail.com>
2025-07-23 10:44:50 +0300

    align pagination

--- src/components/niab/Pagination.tsx
+++ src/components/niab/Pagination.tsx
@@ -12,7 +12,7 @@ function PaginationNIAB({currentPage = 0, nbPages = 0, setCurrentPage}: IPaginat
     function changePageHandler(e: ChangeEvent<unknown>, page: number) {
         setCurrentPage(page)
     }
-    return <Stack spacing={2}>
+    return <Stack spacing={2} sx={{mb: 5, mt: 5}}>
         <Pagination count={nbPages} variant="outlined" shape="rounded" page={currentPage} onChange={changePageHandler} />
     </Stack>
 }
