f65e8361f895bd81df660b472ddee17dcd9349d6
Sergey Shappo <shappoff@gmail.com>
2025-07-26 21:46:55 +0300

    update script

--- prepare_catalogarchivesgov.js
+++ prepare_catalogarchivesgov.js
@@ -1,40 +1,16 @@
 import fs from "fs";
 import path from "path";
 
-export const Belarus_full = JSON.parse(fs.readFileSync(path.resolve(`./catalog-export-20250726155113.json`), 'utf8'));
+export const belarus = JSON.parse(fs.readFileSync(path.resolve(`src/app/catalogarchivesgov/belarus.json`), 'utf8'));
+export const all_merged = JSON.parse(fs.readFileSync(path.resolve(`src/app/catalogarchivesgov/all_merged.json`), 'utf8'));
 
-const all = [
-    ...Belarus_full,
-];
+const others = all_merged.filter((element) => {
+    const isExist = belarus.find((item) => item.title === element.title && item.title2 === element.title2 && item.naId === element.naId);
 
-const res = all.map((item) => {
-    const {scopeAndContentNote, title, digitalObjects, productionDates, naId} = item;
+    return !isExist;
+});
 
-    return {
-        scopeAndContentNote,
-        title,
-        digitalObjects: digitalObjects?.map(({objectUrl, objectFilename}) => ({objectUrl, objectFilename})),
-        productionDates,
-        naId
-    };
-
-}).filter((item) =>
-    item.scopeAndContentNote &&
-    ~item.scopeAndContentNote?.indexOf(',') &&
-    !~item.scopeAndContentNote?.indexOf('Latitude:') &&
-    ~item.scopeAndContentNote?.indexOf('-'))
-    .map((item) => {
-        const [coords, title2] = item.scopeAndContentNote.split('-');
-        const [lat, lng] = coords.trim().split(',');
-        if (!(lat && lng)) {
-            return null
-        }
-        const _geoloc = {lat: lat.trim(), lng: lng.trim()};
-        delete item.scopeAndContentNote;
-        return {...item, _geoloc, title2}
-    }).filter((item) => !!item);
-
-fs.writeFileSync(path.resolve(`./all_merged.json`), JSON.stringify(res, null, 4), {
+fs.writeFileSync(path.resolve(`public/others.json`), JSON.stringify(others, null, 4), {
     encoding: 'utf8',
     flag: 'w'
 });
