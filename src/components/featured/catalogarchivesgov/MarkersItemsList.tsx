1e42afe4983ffea3ec9bc087131fb425536ca7c2
Sergey Shappo <shappoff@gmail.com>
2025-07-27 23:35:13 +0300

    fix bounds

--- src/components/featured/catalogarchivesgov/MarkersItemsList.tsx
+++ src/components/featured/catalogarchivesgov/MarkersItemsList.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import PlaceMarker from "@/components/featured/catalogarchivesgov/PlaceMarker";
+import useMarkersBounds from "@/components/featured/prikhody/useMarkersBounds";
+import BoundsToMapItems from "@/components/featured/prikhody/BoundsToMapItems";
 
 function MarkersItemsList({items}: any) {
-    return items.map((hit: any, index: number) => {
-        return <PlaceMarker key={`${hit.naId}${index}`} hit={hit}/>
-    })
+    const markersBounds = useMarkersBounds(items);
+    return <>
+        <BoundsToMapItems
+            key="BoundsToMapItems"
+            bounds={markersBounds}
+            callback={() => {}}
+        />
+        {
+            items.map((hit: any, index: number) => {
+                return <PlaceMarker key={`${hit.naId}${index}`} hit={hit}/>
+            })
+        }
+    </>
 }
 
 export default MarkersItemsList;
