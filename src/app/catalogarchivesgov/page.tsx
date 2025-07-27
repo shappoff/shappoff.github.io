7d7027f38e823ea8505057df8cd2d672f684d5a8
Sergey Shappo <shappoff@gmail.com>
2025-07-27 23:49:16 +0300

    fix links

--- src/app/catalogarchivesgov/page.tsx
+++ src/app/catalogarchivesgov/page.tsx
@@ -4,6 +4,9 @@ import fs from "fs";
 import {catalogarchivesgovBelarusPath} from "@/components/paths";
 import HomeButton from "@/components/shared/HomeButton";
 import MapWrapper from "@/components/featured/catalogarchivesgov/MapWrapper";
+import Button from '@mui/material/Button';
+import Link from 'next/link'
+import Stack from '@mui/material/Stack';
 
 export const metadata: Metadata = {
     title: "Карта аэрофотосъемки Беларуси времен ВОВ",
@@ -13,7 +16,17 @@ export const metadata: Metadata = {
 export default function CatalogarchivesgovPage() {
     const belarusItems = JSON.parse(fs.readFileSync(catalogarchivesgovBelarusPath, 'utf8'));
     return <>
-        <HomeButton absolute={true} variant={true}/>
+        <Stack spacing={2} direction="row" sx={{position: 'absolute', top: 0, left: 0, zIndex: 999}}>
+            <HomeButton absolute={false} variant={true}/>
+            <Link href="/catalogarchivesgov/smolensk">
+                <Button
+                    variant="contained"
+                    size="small"
+                >
+                    Смоленск
+                </Button>
+            </Link>
+        </Stack>
         <MapWrapper items={belarusItems} />
     </>;
 }
