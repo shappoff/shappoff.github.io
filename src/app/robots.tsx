94e50388b43d3f9e95d6ddd3959a75391bb6c2f6
Sergey Shappо́ <s.shappo@whitebird.io>
2026-04-30 10:46:02 +0300

    update robots.tsx

--- src/app/robots.tsx
+++ src/app/robots.tsx
@@ -1,14 +1,17 @@
 import type { MetadataRoute } from 'next'
 
 export const dynamic = 'force-static';
+const SITE_URL = (process.env.NEXT_PUBLIC_SITE_URL ?? 'https://shappoff.github.io').replace(/\/$/, '');
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/private/',
-        },
-        sitemap: 'https://shappoff.github.io/sitemap.xml',
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+            },
+        ],
+        sitemap: `${SITE_URL}/sitemap.xml`,
+        host: SITE_URL,
     }
 }
