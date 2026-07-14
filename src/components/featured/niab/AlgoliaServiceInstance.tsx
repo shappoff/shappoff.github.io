25c9cc3020f25649d77feac22c69e4e897aa985e
Sergey Shappо́ <s.shappo@whitebird.io>
2026-07-14 13:16:17 +0300

    refactor(niab): Update npm dependencies to latest versions

--- src/components/featured/niab/AlgoliaServiceInstance.tsx
+++ src/components/featured/niab/AlgoliaServiceInstance.tsx
@@ -1,13 +1,25 @@
-import algoliasearch from 'algoliasearch/lite';
+import { liteClient } from 'algoliasearch/lite';
 
 declare const process: any;
 
-const client = algoliasearch(
+const INDEX_NAME = 'foandyniab';
+
+const client = liteClient(
     process.env.NEXT_PUBLIC_NIAB_ALGOLIA_APPLICATION_ID,
     process.env.NEXT_PUBLIC_NIAB_ALGOLIA_SEARCH_ONLY_API_KEY
 );
 
-const algoliaIndex = client.initIndex('foandyniab');
-
+const algoliaIndex = {
+    search: async (query: string, searchParams?: Record<string, unknown>) => {
+        const { results } = await client.searchForHits({
+            requests: [{
+                indexName: INDEX_NAME,
+                query,
+                ...searchParams,
+            }],
+        });
+        return results[0];
+    },
+};
 
 export default algoliaIndex;
